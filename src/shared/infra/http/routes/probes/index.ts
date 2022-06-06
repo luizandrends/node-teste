efde57d340edb61dabbfda8187327a8f60a50a6c
Luiz Andre Nunes da Silva <luizxd234@gmail.com>
2022-06-06 00:21:43 -0300

    chore - First commit

--- src/shared/infra/http/routes/probes/index.ts
+++ src/shared/infra/http/routes/probes/index.ts
@@ -2,12 +2,12 @@ import { Router } from 'express';
 
 const routes = Router();
 
-routes.get("/liveness", (request, response) => {
+routes.get('/liveness', (request, response) => {
   return response.json({ ok: true });
-})
+});
 
-routes.get("/readiness", (request, response) => {
+routes.get('/readiness', (request, response) => {
   return response.json({ ok: true });
-})
+});
 
 export default routes;
