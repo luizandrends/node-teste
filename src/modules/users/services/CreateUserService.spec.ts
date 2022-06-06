eea05e353602a9863f8bf1611657e73ef467ef30
Luiz Andre Nunes da Silva <luizxd234@gmail.com>
2022-06-06 01:25:58 -0300

    chore - Creating createUsersService test

---
+++ src/modules/users/services/CreateUserService.spec.ts
@@ -0,0 +1,40 @@
+import AppError from '@shared/errors/AppError';
+import FakeUsersRepository from '../interfaces/fakes/FakeUsersRepository';
+import CreateUserService from './CreateUserService';
+
+let fakeUsersRepository: FakeUsersRepository;
+let createUserService: CreateUserService;
+
+describe('CreateUserService', () => {
+  beforeEach(() => {
+    fakeUsersRepository = new FakeUsersRepository();
+
+    createUserService = new CreateUserService(fakeUsersRepository);
+  });
+
+  it('should be able to create a new user', async () => {
+    const userData = {
+      name: 'John Doe',
+      email: 'johndoe@example.com',
+      password: '123456',
+    };
+
+    const createUser = await createUserService.execute(userData);
+
+    expect(createUser).toHaveProperty('_id');
+  });
+
+  it('should not be able to create an user with a existent email', async () => {
+    const userData = {
+      name: 'John Doe',
+      email: 'johndoe@example.com',
+      password: '123456',
+    };
+
+    await createUserService.execute(userData);
+
+    await expect(createUserService.execute(userData)).rejects.toBeInstanceOf(
+      AppError
+    );
+  });
+});
